877ccaf5463b0cc80c18af5c325de522178a03c6
Nicholas M. Getchell <getch3028@gmail.com>
2015-10-07 22:10:25 -0400

    Moved files around

--- PSGitLab/PSGitLab.psm1
+++ PSGitLab/PSGitLab.psm1
@@ -1,6 +1,6 @@
 #Get public and private function definition files.
-$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
-$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\ -Include *.ps1 -Recurse -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\ -Include *.ps1 -Recurse -ErrorAction SilentlyContinue )
 
 #Dot source the files
 Foreach($import in @($Public + $Private)) {
