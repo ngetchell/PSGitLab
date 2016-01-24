fbe51062a498e7d417f3daaa66479b0cb1b7d93f
Nicholas M. Getchell <getch3028@gmail.com>
2016-01-23 20:17:01 -0500

    Added one internal and one external function. Check to see if the configuration is working as expected.

---
+++ PSGitLab/Public/Configuration/Test-GitLabAPI.ps1
@@ -0,0 +1,9 @@
+﻿Function Test-GitLabAPI {
+
+    $GitLabConfig = ImportConfig
+    $Domain = $GitLabConfig.Domain
+    $Token = $GitLabConfig.Token
+    
+    $Result = Invoke-WebRequest -Uri "$Domain/api/v3/projects?private_token=$Token"
+    GetGitLabStatusCode $Result.StatusCode
+}
