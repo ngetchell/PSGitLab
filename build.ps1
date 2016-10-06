dbe34cd17e5a7626119a8c64e8592173d5024614
Nicholas M. Getchell <getch3028@gmail.com>
2016-10-06 17:40:37 -0400

    Ignore file and merge psm1.

---
+++ build.ps1
@@ -0,0 +1,54 @@
+# Source: https://github.com/devblackops/POSHOrigin/blob/master/build.ps1
+
+[cmdletbinding()]
+param(
+    [string[]]$Task = 'Test'
+)
+
+function Resolve-Module {
+    [Cmdletbinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string[]]$Name
+    )
+
+    Process {
+        foreach ($ModuleName in $Name) {
+            $Module = Get-Module -Name $ModuleName -ListAvailable
+            Write-Verbose -Message "Resolving Module [$($ModuleName)]"
+            
+            if ($Module) {
+                $Version = $Module | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
+                $GalleryVersion = Find-Module -Name $ModuleName -Repository PSGallery -Verbose:$false | 
+                    Measure-Object -Property Version -Maximum | 
+                    Select-Object -ExpandProperty Maximum
+
+                if ($Version -lt $GalleryVersion) {                                        
+                    Write-Verbose -Message "$($ModuleName) Installed Version [$($Version.tostring())] is outdated. Installing Gallery Version [$($GalleryVersion.tostring())]"
+                    
+                    Install-Module -Name $ModuleName -Verbose:$false -Force
+                    Import-Module -Name $ModuleName -Verbose:$false -Force -RequiredVersion $GalleryVersion
+                }
+                else {
+                    Write-Verbose -Message "Module Installed, Importing [$($ModuleName)]"
+                    Import-Module -Name $ModuleName -Verbose:$false -Force -RequiredVersion $Version
+                }
+            }
+            else {
+                Write-Verbose -Message "[$($ModuleName)] Missing, installing Module"
+                Install-Module -Name $ModuleName -Verbose:$false -Force
+                Import-Module -Name $ModuleName -Verbose:$false -Force -RequiredVersion $Version
+            }
+        }
+    }
+}
+
+Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+'BuildHelpers', 'psake','Pester', 'PSDeploy', 'PSScriptAnalyzer' | Resolve-Module
+
+Set-BuildEnvironment
+
+Invoke-psake -buildFile "$PSScriptRoot\psake.ps1" -taskList $Task -nologo -Verbose:$VerbosePreference
+exit ( [int]( -not $psake.build_success ) )
---
+++ psake.ps1
@@ -0,0 +1,106 @@
+properties {
+    $ModuleName = 'PSGitLab'
+    $projectRoot = $ENV:BHProjectPath
+    if(-not $projectRoot) {
+        $projectRoot = $PSScriptRoot
+    }
+
+    $sut = "$projectRoot\$ModuleName"
+    $tests = "$projectRoot\Tests"
+
+    $psVersion = $PSVersionTable.PSVersion.Major
+}
+
+task default -depends init
+
+task Init {
+    "`nSTATUS: Testing with PowerShell $psVersion"
+    "Build System Details:"
+    Get-Item ENV:BH*
+
+    $modules = 'Pester', 'PSDeploy', 'PSScriptAnalyzer'
+    Import-Module $modules -Verbose:$false -Force
+}
+
+task Test -Depends Init, Analyze, Pester
+
+task Analyze {
+    # Modify PSModulePath of the current PowerShell session.
+    # We want to make sure we always test the development version of the resource
+    # in the current build directory.
+    $origModulePath = $env:PSModulePath
+    $newModulePath = $origModulePath
+    if (($newModulePath.Split(';') | Select-Object -First 1) -ne $projectRoot) {
+        # Add the project root to the beginning if it is not already at the front.
+        $env:PSModulePath = "$projectRoot;$env:PSModulePath"
+    }
+
+    $excludedRules = (
+        'PSAvoidUsingConvertToSecureStringWithPlainText', # For private token information
+        'PSAvoidUsingUserNameAndPassWordParams' # this refers to gitlab users and passwords
+    )
+    $saResults = Invoke-ScriptAnalyzer -Path $sut -Severity Error -ExcludeRule $excludedRules -Recurse -Verbose:$false
+
+    # Restore PSModulePath
+    if ($origModulePath -ne $env:PSModulePath) {
+        $env:PSModulePath = $origModulePath
+    }
+
+    if ($saResults) {
+        $saResults | Format-Table
+        Write-Error -Message 'One or more Script Analyzer errors/warnings where found. Build cannot continue!'
+    }
+}
+
+task Pester {
+    if(-not $ENV:BHProjectPath) {
+        Set-BuildEnvironment -Path $PSScriptRoot\..
+    }
+    Remove-Module $ENV:BHProjectName -ErrorAction SilentlyContinue
+    Import-Module (Join-Path $ENV:BHProjectPath $ENV:BHProjectName) -Force
+
+    $testResults = Invoke-Pester -Path $tests -PassThru
+    if ($testResults.FailedCount -gt 0) {
+        $testResults | Format-List
+        Write-Error -Message 'One or more Pester tests failed. Build cannot continue!'
+    }
+}
+
+task mergePSM1 -depends init {
+    
+    Write-Host "Merging the PSM1"
+
+    $ReleaseDirectory = join-path $projectRoot 'Release'
+    if (Test-Path $ReleaseDirectory) {
+        remove-item -Recurse -Force -Path $ReleaseDirectory
+    }
+
+    #Create Release Folder
+    New-Item -Path $ReleaseDirectory -ItemType Directory | Out-Null
+    
+    #Copy Module Manifest
+    Copy-Item "$projectRoot\$ModuleName\$ModuleName.psd1" -Destination $ReleaseDirectory
+
+    #Copy Formats
+    if (Test-Path "$projectRoot\$ModuleName\$ModuleName.Format.ps1xml") {
+        Copy-Item "$projectRoot\$ModuleName\$ModuleName.Format.ps1xml" -Destination $ReleaseDirectory
+    }
+
+    # Merge PSM1
+    $PSM1Path = "$ReleaseDirectory\$ModuleName.psm1"
+
+    foreach ($Folder in @('Public','Private') ) {
+
+        "##########################" | Add-Content $PSM1Path
+        "#    $Folder Functions    " | Add-Content $PSM1Path
+        "##########################" | Add-Content $PSM1Path
+
+        foreach ($Function in (Get-ChildItem $projectRoot\$ModuleName\$Folder -Recurse -Include *.ps1) ) {
+            Get-Content $Function.Fullname | Add-Content $PSM1Path
+            "`r`n" | Add-Content $PSM1Path
+        }
+
+    }
+
+
+}
