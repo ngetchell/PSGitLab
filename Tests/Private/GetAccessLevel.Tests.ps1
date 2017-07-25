df8450f2cc831bf4e377776b3d2aa1585fefcc31
Nicholas M. Getchell <getch3028@gmail.com>
2017-07-24 21:01:29 -0400

    Fixed bad test
    Added in module scope for GetAccessLevel since it is a private function

--- Tests/Private/GetAccessLevel.Tests.ps1
+++ Tests/Private/GetAccessLevel.Tests.ps1
@@ -5,62 +5,63 @@ Get-Module $ModuleName | Remove-Module
 
 Import-Module $ModuleManifest
 
-Describe 'GetAccessLevel' {
+InModuleScope -ModuleName $ModuleName {
+    Describe 'GetAccessLevel' {
 
-    Context 'Group Permissions' {
-        $Type = 'Group'
+        Context 'Group Permissions' {
+            $Type = 'Group'
 
-        It 'Guest' {
-            GetAccessLevel -Level Guest -Type $Type | Should be 10
+            It 'Guest' {
+                GetAccessLevel -Level Guest -Type $Type | Should be 10
+            }
+
+            It 'Reporter' {
+                GetAccessLevel -Level Reporter -Type $Type | Should be 20
+            }
+
+            It 'Developer' {
+                GetAccessLevel -Level Developer -Type $Type | Should be 30
+            }
+
+            It 'Master' {
+                GetAccessLevel -Level Master -Type $Type | Should be 40
+            }
+
+            It 'Owner' {
+                GetAccessLevel -Level Owner -Type $Type | Should be 50
+            }
+
+            It 'Owner do not throw' {
+                { GetAccessLevel -Level Owner -Type 'Group' -ErrorAction Stop } | Should not throw
+            }
+
         }
 
-        It 'Reporter' {
-            GetAccessLevel -Level Reporter -Type $Type | Should be 20
-        }
+        Context 'Project Permissions' {
+            $Type = 'Project'
 
-        It 'Developer' {
-            GetAccessLevel -Level Developer -Type $Type | Should be 30
-        }
+            It 'Guest' {
+                GetAccessLevel -Level Guest -Type $Type | Should be 10
+            }
 
-        It 'Master' {
-            GetAccessLevel -Level Master -Type $Type | Should be 40
-        }
+            It 'Reporter' {
+                GetAccessLevel -Level Reporter -Type $Type | Should be 20
+            }
 
-        It 'Owner' {
-            GetAccessLevel -Level Owner -Type $Type | Should be 50
-        }
+            It 'Developer' {
+                GetAccessLevel -Level Developer -Type $Type | Should be 30
+            }
 
-        It 'Owner do not throw' {
-            { GetAccessLevel -Level Owner -Type 'Group' -ErrorAction Stop } | Should not throw
-        }
+            It 'Master' {
+                GetAccessLevel -Level Master -Type $Type | Should be 40
+            }
+
+            It 'Owner' {
+                { GetAccessLevel -Level Owner -Type $Type -ErrorAction Stop } | Should throw 'Projects do not have owner permission set. See https://docs.gitlab.com/ce/api/members.html'
+            }
+
+        }    
 
     }
-
-    Context 'Project Permissions' {
-        $Type = 'Project'
-
-        It 'Guest' {
-            GetAccessLevel -Level Guest -Type $Type | Should be 10
-        }
-
-        It 'Reporter' {
-            GetAccessLevel -Level Reporter -Type $Type | Should be 20
-        }
-
-        It 'Developer' {
-            GetAccessLevel -Level Developer -Type $Type | Should be 30
-        }
-
-        It 'Master' {
-            GetAccessLevel -Level Master -Type $Type | Should be 40
-        }
-
-        It 'Owner' {
-            { GetAccessLevel -Level Owner -Type $Type -ErrorAction Stop } | Should throw 'Projects do not have owner permission set. See https://docs.gitlab.com/ce/api/members.html'
-        }
-
-    }    
-
 }
-
 Remove-Module $ModuleName
