546555c2d0fcc46aa000cf4d34c184171cf4a645
Nicholas M. Getchell <getch3028@gmail.com>
2017-08-24 20:47:33 -0400

    * made the auth more generic * fixed up user tests

--- Tests/Integration/Authentication.ps1
+++ Tests/Integration/Authentication.ps1
@@ -1,17 +1,20 @@
-$Domain = 'http://10.1.1.83'
+$Domain = 'http://gitlab:5000'
+
+if ( -not ( Get-Variable -Name GitLabCredential -ErrorAction SilentlyContinue ) ) {
+    $GitlabCredential = Get-Credential
+}
+
 Function Get-GitlabUserPrivateToken {
     [cmdletbinding()]
     param(
-        $Username,
-
-        $Password
+        $GitlabCredential
     )
 
-    $URI = "$Domain/api/v4/session?login=$Username&password=$Password"
+    $URI = "$Domain/api/v4/session?login={0}&password={1}" -f $GitlabCredential.UserName, $GitlabCredential.GetNetworkCredential().Password
     
     Invoke-RestMethod -URI $URI -Method POST | 
         Select-Object -ExpandProperty Private_Token
 }
 
-$PrivateToken = Get-GitlabUserPrivateToken -Username ******* -Password *********
+$PrivateToken = Get-GitlabUserPrivateToken -GitlabCredential $GitlabCredential
 Save-GitLabAPIConfiguration -Domain $Domain -Token $PrivateToken
--- Tests/Integration/Users.Tests.ps1
+++ Tests/Integration/Users.Tests.ps1
@@ -1,61 +1,76 @@
 . $PSScriptRoot\Authentication.ps1
 
-Describe "Get-GitLabUser" -Tag "Integration" {
-    Context "Default User" {
+Describe "User" -Tag "Integration" {
+    Context "Get-GitLabUser" {
         $User = Get-GitLabUser -ID 1
         It "Name" {
             $User.name | Should be 'Administrator'
         }
-
+        
         It "Username" {
             $User.username | Should be 'root'
         }
-
+        
         It "Id" {
             $User.id | Should be 1
         }
-
+        
         It "is_admin" {
             $User.is_admin | Should be $true
         }
-
+        
         It "Email" {
             $User.email | Should be 'admin@example.com'
         }
-
+        
         It "state" {
             $User.State | Should be 'active'
         }
     }
-}
-
-Describe "New-GitLabUser"  -Tag "Integration" {
-
-    Context "Create User" {
-
+    Context "New-GitLabUser" {
+    
         BeforeAll {
             $Parameters = @{
                 Email = 'newuser@gmail.com'
                 Username = 'gitlab_user'
                 Name = 'Test User'
                 Twitter = 'fake_twitter'
-                SkypeID = 'fake skype'
+                SkypeID = 'fakeskype@test.com'
                 LinkedIn = 'linkedINuser'
                 WebsiteURL = 'http://example.com'
             }
             $New = New-GitLabUser -Password 'Example1' -Passthru @Parameters 
             $User = Get-Gitlabuser -ID $new.id
         }
-
+    
         AfterAll {
             $User | Remove-GitLabUser -Confirm:$false
         }
-
+    
         $Parameters.GetEnumerator() | ForEach-Object {
             It $_.Key {
                 $User."$($_.Key)" | Should be $_.Value
             }
         }
+    
+    }
 
+    Context 'Remove-GitLabUser' {
+        $User = Get-GitLabUser -Username gitlab_user
+        It "User Doesn't exist" {
+            $User.count | should benullorempty
+        }
+
+        $User = New-GitlabUser -Password 'Exmaple1' @Parameters -Passthru
+        it 'should be created' {
+            $User.username | Should be 'gitlab_user'
+        }
+
+        Remove-GitLabUser -Username $User.Username -Confirm:$false
+        $User = Get-GitLabUser -Username gitlab_user
+
+        it 'should be gone again' {
+            $User.count | should benullorempty
+        }
     }
 }
