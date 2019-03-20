88e88b3b9ebc10a1df981268857723e2a297a439
Nicholas M. Getchell <getch3028@gmail.com>
2019-03-19 20:41:59 -0400

    migrate to invoke-build

--- psgitlab.build.ps1
+++ psgitlab.build.ps1
@@ -71,7 +71,7 @@ Task Pester -inputs { Get-ChildItem -Path "$projectRoot\$ModuleName\","$projectR
     # AppVeyor NUnitXml Upload
     # Source: https://github.com/pester/Pester/wiki/Showing-Test-Results-in-CI-(TeamCity,-AppVeyor)
 
-    $testResults = Invoke-Pester -Path $tests -PassThru -OutputFile $PesterResultsFile
+    $testResults = Invoke-Pester -Path $tests -PassThru -OutputFile $PesterResultsFile -ExcludeTag 'Integration'
 
     # Upload to AppVeyor 
     if ( $env:BuildBuildSystem -eq 'AppVeyor' ) {
@@ -81,6 +81,63 @@ Task Pester -inputs { Get-ChildItem -Path "$projectRoot\$ModuleName\","$projectR
     if ($testResults.FailedCount -gt 0) {
         $testResults | Format-List
         throw 'One or more Pester tests failed. Build cannot continue!'
+    }    
+}
+
+# Synopsis: Prepair Integration Tests
+Task Pre-Integration  Build, {
+    $ProgressPreference = 'Silent'
+    $WarningPreference = 'Silent'
+    
+    
+    $Token = Get-Random
+    $ContainerName = 'gitlab'
+    $URI = 'http://localhost'
+    
+    Write-Host "Checking Status of gitlab container"
+    
+    $Inspect = docker inspect $ContainerName | ConvertFrom-Json
+    if ( $Inspect.State.Status -ne 'running' ) {
+        Write-Host "Starting $ContainerName"
+        docker run --detach --hostname gitlab.example.com --env GITLAB_OMNIBUS_CONFIG="external_url '$URI'; gitlab_rails['lfs_enabled'] = true; gitlab_rails['initial_root_password'] = 'longterriblepassword!' "     --publish 4433:433 --publish 80:80 --publish 2222:22 --name $ContainerName --restart always gitlab/gitlab-ce:latest
+    }
+    
+    Write-Host "Waiting for $ContainerName to start web server."
+    $StartTime = Get-Date
+    do {
+        Start-Sleep -Seconds 5
+        $StatusCode = docker exec -ti gitlab curl -s -o /dev/null -w "%{http_code}" localhost
+        $Timespent = (Get-Date).Subtract($StartTime)
+        "Time Elapsed: {0}" -f [math]::Round($Timespent.TotalSeconds)
+    } while ( $StatusCode -ne 302  -or $Timespent.Minutes -gt 10 )
+    Write-Host "Web server started and is properly redirecting."
+    
+    Write-Host "Setting Personal Access Token to $Token"
+    docker exec -ti -u git gitlab gitlab-rails r "token = PersonalAccessToken.new(user: User.where(id: 1).first, name: 'temp token13', token: '$Token', scopes: ['api','read_user','sudo']); token.save"'!'
+    
+    Install-Module PSGitLab -Scope CurrentUser -Force
+    
+    Save-GitLabAPIConfiguration -Token $Token -Domain 'http://localhost'
+    
+    Test-GitLabAPI  
+}
+
+# Synopsis: Integration Tests
+Task Integration  Build,Pre-integration {
+    if(-not $ENV:BuildProjectPath) {
+        Set-BuildEnvironment -Path $PSScriptRoot\..
+    }
+    Remove-Module $ENV:BuildProjectName -ErrorAction SilentlyContinue
+    Import-Module (Join-Path $ENV:BuildProjectPath $ENV:BuildProjectName) -Force
+
+    # AppVeyor NUnitXml Upload
+    # Source: https://github.com/pester/Pester/wiki/Showing-Test-Results-in-CI-(TeamCity,-AppVeyor)
+
+    $testResults = Invoke-Pester -Path $tests -PassThru -OutputFile $PesterResultsFile -Tag 'Integration'
+
+    if ($testResults.FailedCount -gt 0) {
+        $testResults | Format-List
+        throw 'One or more Integration tests failed. Build cannot continue!'
     }    
 }
 
